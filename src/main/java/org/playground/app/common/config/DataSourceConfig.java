b9e8bcb815b1e546c4d4cec7b802fe498e857790
danny <gryb809@gmail.com>
2020-12-30 00:58:01 +0900

    :wrench: DataSource 설정

---
+++ src/main/java/org/playground/app/common/config/DataSourceConfig.java
@@ -0,0 +1,26 @@
+package org.playground.app.common.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource("classpath:/application.yml")
+public class DataSourceConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("spring.datasource.hikari")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+}
